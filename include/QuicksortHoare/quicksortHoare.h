ec03e28ac2c5cfbb70f5d71ab85125f88c7157dd
Apisteftos <kosagglis@gmail.com>
2024-03-17 22:15:15 +0100

    update

--- include/QuicksortHoare/quicksortHoare.h
+++ include/QuicksortHoare/quicksortHoare.h
@@ -1,21 +1,23 @@
 #ifndef QUICKSORTHOARE_H
 #define QUICKSORTHOARE_H
 #include <iostream>
-
+#include <vector>
 
 class QuickSortHoare{
 
 private:
 
-
+    int partitionHoare(std::vector<int> &arr, int lo, int hi);
 
 public: 
 
     QuickSortHoare();
 
+    void quickSortHoare(std::vector<int> &arr, int lo, int hi);
 
-    
+    void swap(int &arr1, int &arr2);
 
+    ~QuickSortHoare();
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,28 @@ int main(){
     /*          Quick Sort Hoare algorithm                 */
     /*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-    std::vector<int> arr2 = {7, 8, 1, 0, 6, 10, 34, 5, 73, 12, 44};
+    std::vector<int> arr2 = {4, 333, 9, 0, 1, 55, 77, 38, 3, 5, 23};
     int hi2 = arr2.size();
 
     std::cout <<"##################### Quicksort Hoare Algorithm ######################" << std::endl;
     
+    std::cout << "\nUnsorted array: ";
+    for(int i = 0; i < hi2; i++){
+        std::cout << arr2[i] << " ";
+    }
+
+    QuickSortHoare quicksrthoare = QuickSortHoare();
+
+    quicksrthoare.quickSortHoare(arr2, 0, hi2 - 1);
     
+    // Print the sorted array
+    std::cout << "\nSorted array: ";
+    for (int i = 0; i < arr2.size(); i++) {
+        std::cout << arr2[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
 
 
     /*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/
